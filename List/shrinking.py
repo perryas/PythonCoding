aac21a5838087e89a2687ed0fcc5ca12c68bda2a
Ayana Perry <78577866+perryas@users.noreply.github.com>
2022-04-26 23:11:36 -0400

    More data structure practice

---
+++ List/shrinking.py
@@ -0,0 +1,95 @@
+"""_summary_
+    Shrinking a List: Remove
+We can remove elements in a list using the .remove() Python method.
+
+Suppose we have a filled list called shopping_line that represents a line at a grocery store:
+
+shopping_line = ["Cole", "Kip", "Chris", "Sylvana"]
+We could remove "Chris" by using the .remove() method:
+
+shopping_line.remove("Chris")
+ 
+print(shopping_line)
+If we examine shopping_line, we can see that it now doesn’t contain "Chris":
+
+["Cole", "Kip", "Sylvana"]
+We can also use .remove() on a list that has duplicate elements.
+
+Only the first instance of the matching element is removed:
+
+# Create a list
+shopping_line = ["Cole", "Kip", "Chris", "Sylvana", "Chris"]
+ 
+# Remove a element
+shopping_line.remove("Chris")
+print(shopping_line)
+Will output:
+
+["Cole", "Kip", "Sylvana", "Chris"]
+Let’s practice using the .remove() method to remove elements from a list.
+
+Instructions
+1.
+We have decided to get into the grocery store business. Our manager Calla has decided to store all the inventory purchases in a list to help track what products need to be ordered.
+
+Let’s create a list called order_list with the following values (in this particular order):
+
+"Celery", "Orange Juice", "Orange", "Flatbread"
+
+Print order_list to see the current list.
+
+
+Stuck? Get a hint
+2.
+We are in luck! We actually found a spare case of "Flatbread" in our back storage. We won’t need to order it anymore. Let’s remove it from order_list using the .remove() method.
+
+Print order_list to see the current list.
+
+
+Stuck? Get a hint
+3.
+Our store has grown to be a huge success! We decided to open a second store and require a new order list. Calla has done us the favor of putting one together.
+
+Create a new list called new_store_order_list and assign it the following values (in order):
+
+"Orange", "Apple", "Mango", "Broccoli", "Mango"
+
+Note: Our second store is going to need two orders of mangos so the value is duplicated.
+
+Print new_store_order_list to see the current list.
+
+
+Stuck? Get a hint
+4.
+We are in luck again! We actually found a spare case of "Mango" in our back storage.
+
+We won’t be needing to place two orders anymore.
+
+Let’s remove it from new_store_order_list using the .remove() method.
+
+Print new_store_order_list to see the current list.
+
+
+Stuck? Get a hint
+5.
+Calla ran to tell us some important news! She asked us to remove "Onions" from our new new_store_order_list. If we double-check our list, we will notice we don’t have "Onions" on our list.
+
+Let’s see what happens when we try to remove an item that does not exist.
+
+Call the .remove() method with the value of "Onions" on our new_store_order_list list.
+"""
+
+# Your code below: 
+order_list = ["Celery", "Orange Juice", "Orange", "Flatbread"]
+order_list.remove("Flatbread")
+
+print(order_list)
+
+new_store_order_list = ["Orange", "Apple", "Mango", "Broccoli", "Mango"]
+
+print(new_store_order_list)
+
+new_store_order_list.remove("Mango")
+print(new_store_order_list)
+
+new_store_order_list.remove("Onions")
---
+++ List/twodimen.py
@@ -0,0 +1,38 @@
+"""_summary_
+Two-Dimensional (2D) Lists
+We’ve seen that the items in a list can be numbers or strings. Lists can contain other lists! We will commonly refer to these as two-dimensional (2D) lists.
+
+Once more, let’s look at a class height example:
+
+Noelle is 61 inches tall
+Ava is 70 inches tall
+Sam is 67 inches tall
+Mia is 64 inches tall
+Previously, we saw that we could create a list representing both Noelle’s name and height:
+
+noelle = ["Noelle", 61]
+We can put several of these lists into one list, such that each entry in the list represents a student and their height:
+
+heights = [["Noelle", 61], ["Ava", 70], ["Sam", 67], ["Mia", 64]]
+We will often find that a two-dimensional list is a very good structure for representing grids such as games like tic-tac-toe.
+
+#A 2d list with three lists in each of the indices. 
+tic_tac_toe = [
+            ["X","O","X"], 
+            ["O","X","O"], 
+            ["O","O","X"]
+]
+Let’s practice creating our own 2D list!
+
+Instructions
+1.
+A new student named "Vik" has joined our class. Vik is 68 inches tall. Add a sublist to the end of the heights list that represents Vik and his height.
+
+
+Stuck? Get a hint
+2.
+Create a two-dimensional list called ages where each sublist contains a student’s name and their age. Use the following data:
+
+"Aaron" is 15
+"Dhruti" is 16
+"""
